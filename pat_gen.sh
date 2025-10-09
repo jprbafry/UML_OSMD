9d8a81d6e22d94b0c61de24997c347a43df54ae0
Patricia Baladjay <patriciaanna.baladjay@afry.com>
2025-10-09 10:21:12 +0200

    Activity Diagram for GUI

--- pat_gen.sh
+++ pat_gen.sh
@@ -1,9 +1,9 @@
-cd uml
+cd uml/pat
 pwd
 java -jar ~/plantuml/plantuml.jar -tsvg *.txt # & PID=$!; sleep 10; kill $PID
 java -jar ~/plantuml/plantuml.jar *.txt # & PID=$!; sleep 10; kill $PID
-mv *.svg ../img/svg
-mv *.png ../img/png
+mv *.svg ../../img/svg
+mv *.png ../../img/png
 cd ..
 
 
