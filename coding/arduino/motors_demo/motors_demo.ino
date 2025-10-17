22e8851539702edf89efcc36c97ea68a645b3240
Ango01 <aniagl@kth.se>
2025-10-17 13:06:30 +0200

    Integration GUI and Motor Control ... first step

--- coding/arduino/motors_demo/motors_demo.ino
+++ coding/arduino/motors_demo/motors_demo.ino
@@ -15,6 +15,7 @@
 #define LIGHT_RAD_DIR 31
 
 
+
 // === Constants ===
 // TODO: Change if necessary (how many steps the motor does per degree depends on the setup)
 const float DET_AZ_STEPS_PER_DEG = 12.0;
@@ -24,7 +25,7 @@ const float LIGHT_RAD_STEPS_PER_DEG = 10.0;
 
 
 const unsigned int NUM_MOTORS = 4; // How many motors do we have
-const unsigned int STEP_DELAY_US = 500;  // Motor speed control
+const unsigned int STEP_DELAY_US = 500;  
 
 
 
@@ -61,9 +62,17 @@ Motor motors[NUM_MOTORS];
 // Thread controller manages all threads
 ThreadController controller = ThreadController();
 
+
+#define MOTION_PERIOD 20
+#define RECEPTION_PERIOD 5
+#define TRANSMISSION_PERIOD 40
+
 // --- Threads ---
 Thread threadReceive = Thread();
-Thread threadUpdate  = Thread();
+Thread threadMoveM1  = Thread();
+Thread threadMoveM2  = Thread();
+Thread threadMoveM3  = Thread();
+Thread threadMoveM4  = Thread();
 Thread threadSend    = Thread();
 
 // --- Shared variables ---
@@ -80,13 +89,20 @@ const float STEP_LIGHT    = 0.1;
 const float STEP_DETECTOR = 0.2;
 
 // === Function Prototypes ===
-void receiveTask();
-void updateTask();
-void sendTask();
 void processInput(String msg);
-void move_to_absolute(MotorAxis axis, float target_angle);
-void reset_position();
+void move_to_absolute(MotorAxis axis, float target_angle, float max_nb_steps);
 Motor* get_motor(MotorAxis axis);
+
+
+void receiveTask();
+
+void moveM1Task();
+void moveM2Task();
+void moveM3Task();
+void moveM4Task();
+
+void sendTask();
+
 
 void setup() {
   Serial.begin(115200);
@@ -97,10 +113,11 @@ void setup() {
 
 
   // initialize motor table (match pins and initial 8 degrees)
-  motors[DETECTOR_AZ]  = {"DET_AZ",  DET_AZ_STEP,  DET_AZ_DIR};
-  motors[DETECTOR_RAD] = {"DET_RAD", DET_RAD_STEP, DET_RAD_DIR};
-  motors[LIGHT_AZ]     = {"LIGHT_AZ",LIGHT_AZ_STEP, LIGHT_AZ_DIR};
-  motors[LIGHT_RAD]    = {"LIGHT_RAD",LIGHT_RAD_STEP, LIGHT_RAD_DIR};
+  motors[DETECTOR_AZ]  = {"DET_AZ",  DET_AZ_STEP,  DET_AZ_DIR, DET_AZ_STEPS_PER_DEG,  long(8 * DET_AZ_STEPS_PER_DEG)};
+  motors[DETECTOR_RAD] = {"DET_RAD", DET_RAD_STEP, DET_RAD_DIR, DET_RAD_STEPS_PER_DEG, long(8 * DET_RAD_STEPS_PER_DEG)};
+  motors[LIGHT_AZ]     = {"LIGHT_AZ",LIGHT_AZ_STEP, LIGHT_AZ_DIR, LIGHT_AZ_STEPS_PER_DEG, long(8 * LIGHT_AZ_STEPS_PER_DEG)};
+  motors[LIGHT_RAD]    = {"LIGHT_RAD",LIGHT_RAD_STEP, LIGHT_RAD_DIR, LIGHT_RAD_STEPS_PER_DEG, long(8 * LIGHT_RAD_STEPS_PER_DEG)};
+
 
   // Setup pins and encoders
   for (int i = 0; i < NUM_MOTORS; ++i) {
@@ -112,17 +129,28 @@ void setup() {
 
   // --- Setup threads ---
   threadReceive.onRun(receiveTask);
-  threadReceive.setInterval(5);   // check serial every 5 ms
+  threadReceive.setInterval(RECEPTION_PERIOD);
 
-  threadUpdate.onRun(updateTask);
-  threadUpdate.setInterval(20);   // update currents every 10 ms
+  threadMoveM1.onRun(moveM1Task);
+  threadMoveM1.setInterval(MOTION_PERIOD); 
+  threadMoveM2.onRun(moveM2Task);
+  threadMoveM2.setInterval(MOTION_PERIOD); 
+  threadMoveM3.onRun(moveM3Task);
+  threadMoveM3.setInterval(MOTION_PERIOD);
+  threadMoveM4.onRun(moveM4Task);
+  threadMoveM4.setInterval(MOTION_PERIOD);
 
   threadSend.onRun(sendTask);
-  threadSend.setInterval(40);     // send currents every 40 ms
+  threadSend.setInterval(TRANSMISSION_PERIOD);
+
+
 
   // Add to controller
   controller.add(&threadReceive);
-  controller.add(&threadUpdate);
+  controller.add(&threadMoveM1);
+  controller.add(&threadMoveM2);
+  controller.add(&threadMoveM3);
+  controller.add(&threadMoveM4);
   controller.add(&threadSend);
 }
 
@@ -138,14 +166,23 @@ Motor* get_motor(MotorAxis axis) {
     return &motors[(int)axis];
 }
 
-void move_to_absolute(MotorAxis axis, float target_angle) {
+void move_to_absolute(MotorAxis axis, float target_angle, float max_nb_steps) {
 
     Motor* m = get_motor(axis);
   
     long target_steps = lround(target_angle * m->steps_per_deg);
     long step_diff = target_steps - m->current_steps;
     bool direction = (step_diff >= 0);
-    long move_steps = abs(step_diff);
+    long move_steps = min(abs(step_diff), max_nb_steps);
+
+    // // 🔹 Print move_steps if positive
+    // if (move_steps > 0) {
+    //     Serial.print("Moving ");
+    //     Serial.print(m->name);
+    //     Serial.print(": ");
+    //     Serial.print(move_steps);
+    //     Serial.println(" steps");
+    // }
     
     digitalWrite(m->dir_pin, direction ? HIGH : LOW);
     for (long i = 0; i < move_steps; i++) {
@@ -157,33 +194,16 @@ void move_to_absolute(MotorAxis axis, float target_angle) {
   
     long starting_pos = m->current_steps;
     m->current_steps = target_steps;
-  
-    // Serial.print("Moved ");
-    // Serial.print(m->name);
-    // Serial.print(" from ");
-    // Serial.print(lround((float)starting_pos / m->steps_per_deg));
-    // Serial.print(" to ");
-    // Serial.print(target_angle, 2);
-    // Serial.print(" degrees. (");
-    // Serial.print(move_steps);
-    // Serial.println(" steps)");
-    // Serial.println("OK");
 }
-
-// Update current values toward desired values
-void moveTo(float &cur, float des, float step) {
-    if (abs(cur - des) < step)
-      cur = des;
-    else if (cur < des)
-      cur += step;
-    else
-      cur -= step;
-};
-
 
 
 // --- Helper to parse 4 comma-separated floats ---
 void processInput(String msg) {
+  
+    // Print the raw input message
+    Serial.print("Arduino Received: ");
+    Serial.println(msg);
+    
     float vals[4];
     int lastIndex = 0;
     int commaIndex = 0;
@@ -228,22 +248,26 @@ void receiveTask() {
   }
 }
 
-
-
-void updateTask() {
-  moveTo(cur_light_azi, des_light_azi, STEP_LIGHT);
-  moveTo(cur_light_pol, des_light_pol, STEP_LIGHT);
-  moveTo(cur_detector_azi, des_detector_azi, STEP_DETECTOR);
-  moveTo(cur_detector_pol, des_detector_pol, STEP_DETECTOR);
+void moveM1Task () {
+    move_to_absolute(LIGHT_RAD, des_light_pol, LIGHT_RAD_STEPS_PER_DEG);
+}
+void moveM2Task () {
+    move_to_absolute(DETECTOR_RAD, des_detector_pol, DET_RAD_STEPS_PER_DEG);
+}
+void moveM3Task () {
+    move_to_absolute(DETECTOR_AZ, des_detector_azi, DET_AZ_STEPS_PER_DEG);
+}
+void moveM4Task () {
+    move_to_absolute(LIGHT_AZ, des_light_azi, LIGHT_AZ_STEPS_PER_DEG);
 }
 
-// Send current values back to Raspberry Pi
+// Send desired values back to Raspberry Pi
 void sendTask() {
-  Serial.print(cur_light_azi, 1);
+  Serial.print(des_light_azi, 1);
   Serial.print(',');
-  Serial.print(cur_light_pol, 1);
+  Serial.print(des_light_pol, 1);
   Serial.print(',');
-  Serial.print(cur_detector_azi, 1);
+  Serial.print(des_detector_azi, 1);
   Serial.print(',');
-  Serial.println(cur_detector_pol, 1);
+  Serial.println(des_detector_pol, 1);
 }
